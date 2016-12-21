e14c2c9837b3a3ce390618ef511787e85c70c35a
Sean Kau <skau@acm.org>
2016-12-20 20:49:26 -0800

    single file

--- main.py
+++ main.py
@@ -4,31 +4,23 @@ import os
 import os.path
 import sqlite3
 import sys
-import indexer.db as db
+from db import db
 
 parser = argparse.ArgumentParser(description='Index a folder of files.')
-parser.add_argument('-a', action='store_true')
+parser.add_argument('-a', action='store_true', help='if provided, the database file will be created')
+parser.add_argument('--root', action='store', required=True, help='provide the root path')
+args = parser.parse_args()
 
-args = sys.argv
-if len(args) != 2:
-  print("missing file name")
-  sys.exit(1)
+path = args.root
 
 conn = sqlite3.connect('index.db')
-
-if sys.argv[1] == '-a':
-	db.createTable(conn)
-	print("tables created")
-	sys.exit(0)
-
-path = os.path.abspath(sys.argv[1])
 
 is_dir = os.path.isdir(path)
 if is_dir:
 	print("Directories not currently supported")
 	sys.exit(1)
 
-stats = readFile(path)
+stats = db.readFile(path)
 f = os.path.split(path)
 parent_dir = os.path.split(f[0])[1]
 split_name = os.path.splitext(f[1])
