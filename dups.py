accea88dfc7bbf6c8bbf19c81e7ea0e8b3751f95
Sean Kau <skau@acm.org>
2016-12-23 12:19:16 -0800

    do incremental deletions

--- dups.py
+++ dups.py
@@ -15,13 +15,26 @@ def printDups(conn, limit):
         coll.append(records)
     return coll
 
+def deleteDups(conn):
+    coll = printDups(conn, 20)
+    for records in coll:
+        records.pop(0)
+        keys = [r.key for r in records]
+        print('Duplicates to be deleted: %s' % keys)
+        res = db.deleteFiles(conn, keys)
+        print(res)
+    print(db.countFiles(conn))
+    return len(coll)
+
 parser = argparse.ArgumentParser(description='Remove duplicate records.')
 parser.add_argument('--scan', action='store_true', help='print duplicate records')
 parser.add_argument('--delete', action='store_true', help='remove duplicate records')
 parser.add_argument('--dups', action='store_true', help='list files with duplicate hashes')
 args = parser.parse_args()
 
+print("WELCOME TO DUPS")
 conn = sqlite3.connect('index.db')
+print("SQLITE DB LOADED")
 
 if args.scan:
     coll = printDups(conn, 1000)
@@ -36,14 +49,14 @@ if args.scan:
     sys.exit(0)
 
 if args.delete:
-    coll = printDups(conn, 2000)
-    for records in coll:
-        records.pop(0)
-        keys = [r.key for r in records]
-        print('Duplicates to be deleted: %s' % keys)
-        res = db.deleteFiles(conn, keys)
-        print(res)
-    print(db.countFiles(conn))
+    for i in range(0,1000):
+        print("Start iteration %d" % i)
+        changes = deleteDups(conn)
+        if changes == 0:
+            print("Out of dups!")
+            break
+        if i % 5 == 0:
+            conn.commit()
     conn.commit()
     sys.exit(0)
 
